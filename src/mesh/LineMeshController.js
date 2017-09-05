bdb5074039f58317420a1a97fa8a1412f08cffc4
giautm <giau.tran@tiki.vn>
2017-09-06 06:33:35 +0700

    Using TRIANGLE_STRIP instead of TRIANGLES

--- src/mesh/LineMeshController.js
+++ src/mesh/LineMeshController.js
@@ -3,8 +3,6 @@ import MeshController from './MeshController';
 import { VertexAttrib } from '../shader/Shader';
 
 const VERTICES_IN_SEGMENT = 2;
-const DATASIZE_OF_SEGMENT = VERTICES_IN_SEGMENT * 2; // multiply 2 for x and y
-const INDICIES_IN_SEGMENT = 6;
 
 /**
  * https://github.com/facebook/react-native/issues/14838#issuecomment-318148455
@@ -29,9 +27,6 @@ class LineMeshController extends MeshController {
     const vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
-    const indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
     gl.enableVertexAttribArray(VertexAttrib.Position);
     gl.vertexAttribPointer(VertexAttrib.Position,
       2, gl.FLOAT, false, 0, 0);
@@ -42,13 +37,9 @@ class LineMeshController extends MeshController {
       const gl = this.getContextGL();
 
       const vertexData = new Float32Array(getVertexData());
-      const indiesData = new Uint16Array(getIndiesData(
-        vertexData.length / DATASIZE_OF_SEGMENT));
 
       gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
-      gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indiesData, gl.STATIC_DRAW);
-
-      gl.drawElements(gl.TRIANGLES, indiesData.length, gl.UNSIGNED_SHORT, 0);
+      gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexData.length / VERTICES_IN_SEGMENT);
     }
   };
 }
